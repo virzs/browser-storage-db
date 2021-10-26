1762446a615af947df1591b525ca1d965d690236
virzs <2369310227@qq.com>
2021-10-26 22:31:14 +0800

    :wrench: 修改配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "none",
+  mode: "development",
   entry: "./src/index.ts",
   optimization: {
     minimize: true,
@@ -11,17 +11,23 @@ module.exports = {
     rules: [
       {
         test: /\.ts?$/,
-        use: [{ loader: "ts-loader" }],
+        use: [{ loader: "babel-loader" }, { loader: "ts-loader" }],
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: [".ts"],
+    extensions: [".ts", ".js"],
   },
   plugins: [new CleanWebpackPlugin()],
   output: {
     filename: "index.js",
+    library: {
+      name: "StorageDB",
+      type: "umd",
+      export: "default",
+      umdNamedDefine: true,
+    },
     path: path.resolve(__dirname, "lib"),
   },
 };
